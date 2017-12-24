b26b244e0aa720b0b1762a8d6634c0e465a8e139
Jordi Böhme <jordi@boehme-lopez.de>
2017-12-24 03:02:29 +0100

    Adjust Probing

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -700,7 +700,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 32   // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -12  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.9 // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.1 // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
@@ -714,7 +714,7 @@
 // The number of probes to perform at each point.
 //   Set to 2 for a fast/slow probe, using the second probe result.
 //   Set to 3 or more for slow probes, averaging the results.
-//#define MULTIPLE_PROBING 2
+#define MULTIPLE_PROBING 2
 
 /**
  * Z probes require clearance when deploying, stowing, and moving between
